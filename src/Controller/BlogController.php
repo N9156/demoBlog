b2c3a0490abe8b79f1508683c4d645973671825e
NathalieP <nathaliepichon.ca@gmail.com>
2020-06-25 17:28:59 +0200

    developpement insertion + modif articles + dev formulaire

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use DateTime;
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistance\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
@@ -39,10 +43,60 @@ class BlogController extends AbstractController
 
     /**
      * @Route("/blog/new", name="blog_create")
+     * @Route("/blog/{id}/edit",name="blog_edit")
      */
-    public function create()
+    public function create(Article $article =null,Request $request, EntityManagerInterface $manager)//25 06 11:38 methode qui permet de supprimer inserer des données
     {
-        return $this->render('blog/create.html.twig');
+        //dump($request);
+        /*if($request->request->count() > 0)
+        {
+            $article = new Article;
+            $article->setTitle($request->request->get('title'))
+                     ->setContent($request->request->get('content'))
+                     ->setImage($request->request->get('image'))
+                     ->setCreatedAt(new DateTime());
+            $manager->persist($article);
+            $manager->flush();//excute l'insertion en bdd
+            dump($article);
+            return $this->redirectToRoute('blog_show',[
+                    'id'=> $article->getId()
+            ]);
+
+        }*/
+        /*->add('title',TextType::class, [
+                         'attr' =>[ 
+                             'placeholder'=>"Saisir le titre de l'article",
+                             'class' => "col-md-6 mx-auto"
+                         ]
+                     ])*/
+        //autre methode de création de formulaire
+        //$article =new Article;//17h
+        /*$article->setTitle("Titre Titre prérempli")
+                ->setContent("Contenu prérempli");*/
+        if(!$article){
+            $article=new Article;
+        }
+        $form = $this->createFormBuilder($article)//this objet du controleur
+                     ->add('title')
+                     ->add('content')
+                     ->add('image')
+                     ->getForm();
+        $form->handleRequest($request);//methode qui envoie dans l'objet article les setters?handleRequest methode de l'objet form
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $article->setCreatedAt(new \DateTime);
+            $manager->persist($article);
+            $manager->flush();
+            dump($article);
+            return $this->redirectToRoute('blog_show',[
+                'id' =>$article->getId()
+                ]);
+
+        }
+        return $this->render('blog/create.html.twig',[
+            'formArticle' =>$form->createView()
+        ]);
+        dump($article);
     }
 
     //show() : methode permettant de voir le detail d'un article
