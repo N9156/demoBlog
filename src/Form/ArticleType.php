db9c2c5ea5c307d7a9dbb70c8dea35e40f935ed7
NathalieP <nathaliepichon.ca@gmail.com>
2020-06-30 17:26:20 +0200

    inscription + connexion + security.yaml

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -3,8 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ArticleType extends AbstractType
@@ -13,6 +15,10 @@ class ArticleType extends AbstractType
     {
         $builder
             ->add('Title')
+            ->add('category',EntityType::class,[ //on ajoute un champ qui permettra de selectionner une categorie d'un article
+                'class' => Category::class,
+                'choice_label' =>'title'
+            ])
             ->add('content')
             ->add('image')
             
